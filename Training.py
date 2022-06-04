5d357004d478afa43076d846d4d559f7cd2f65e7
Eva Panjaitan <evanopalina97@gmail.com>
2022-06-04 13:54:03 +0700

    Initial Commit add Sesi 1

---
+++ Training.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  4 09:49:55 2022
+
+@author: 088922
+"""
+
+print(10)
+print(type(10))
+print(4.2)
+print(type(4.2))
+print(4.)
+print("Haktiv8")
+print(type("Haktiv8"))
+n=300
+a=10
+t=20
+n=(a*t)/2
---
+++ Training1.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  4 10:39:05 2022
+
+@author: 088922
+"""
+
+var=23.5
+print(var)
+var="Now I'm strong"
+print(var)
+name = "Haktive8"
+Age = 54
+has_laptop = True
+print(name,Age,has_laptop)
+age = 1
+Age = 2
+aGe = 3
+AGE = 4
+a_g_e = 5
+_age = 6
+age_ = 7
+_AGE_ = 8
+print(age, Age,aGe, AGE, a_g_e, _age, age_, _AGE_)
+a = 10
+b = 20
+a + b
+print (a+b)
+a = 10
+b = 20
+a + b - 5
+print(a+b-5)
+a = 4
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+# + Operators
+s = 'foo'
+t = 'bar'
+u = 'baz'
+
+print(s + t)
+
+print(s + t + u)
+
+
+print('Hacktiv8 ' + 'PTP')
